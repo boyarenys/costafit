42aacb0713608acc779cec74bddbc6326bc8c10b
scordero <boyarenys@hotmail.com>
2024-04-29 13:22:57 +0200

    test13

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,147 +1,88 @@
-$(document).ready(function () {
+$(function () {
+    const select = (el, all = false) => {
+        el = el.trim();
+        return all ? [...document.querySelectorAll(el)] : document.querySelector(el);
+    };
 
-    (function () {
-        "use strict";
-
-        /**
-         * Easy selector helper function
-         */
-        const select = (el, all = false) => {
-            el = el.trim()
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all);
+        if (selectEl) {
             if (all) {
-                return [...document.querySelectorAll(el)]
+                selectEl.forEach(e => e.addEventListener(type, listener));
             } else {
-                return document.querySelector(el)
+                selectEl.addEventListener(type, listener);
             }
         }
+    };
 
-        /**
-         * Easy event listener function
-         */
-        const on = (type, el, listener, all = false) => {
-            let selectEl = select(el, all)
+    const scrollto = el => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
 
-            if (selectEl) {
-                if (all) {
-                    selectEl.forEach(e => e.addEventListener(type, listener))
-                } else {
-                    selectEl.addEventListener(type, listener)
-                }
+    on('click', '.mobile-nav-toggle', function () {
+        select('#navbar').classList.toggle('navbar-mobile');
+        this.classList.toggle('bi-list bi-x');
+    });
+
+    on('click', '#navbar .nav-link', function (e) {
+        let section = select(this.hash);
+        if (section) {
+            e.preventDefault();
+            let navbar = select('#navbar');
+            let header = select('#header');
+            let sections = select('section', true);
+            let navlinks = select('#navbar .nav-link', true);
+            navlinks.forEach(item => item.classList.remove('active'));
+            this.classList.add('active');
+            if (navbar.classList.contains('navbar-mobile')) {
+                navbar.classList.remove('navbar-mobile');
+                let navbarToggle = select('.mobile-nav-toggle');
+                navbarToggle.classList.toggle('bi-list bi-x');
             }
-        }
-
-        /**
-         * Scrolls to an element with header offset
-         */
-        const scrollto = (el) => {
-            window.scrollTo({
-                top: 0,
-                behavior: 'smooth'
-            })
-        }
-
-        /**
-         * Mobile nav toggle
-         */
-        on('click', '.mobile-nav-toggle', function (e) {
-            select('#navbar').classList.toggle('navbar-mobile')
-            this.classList.toggle('bi-list')
-            this.classList.toggle('bi-x')
-        })
-
-        /**
-         * Scrool with ofset on links with a class name .scrollto
-         */
-        on('click', '#navbar .nav-link', function (e) {
-            let section = select(this.hash)
-            if (section) {
-                e.preventDefault()
-
-                let navbar = select('#navbar')
-                let header = select('#header')
-                let sections = select('section', true)
-                let navlinks = select('#navbar .nav-link', true)
-
-                navlinks.forEach((item) => {
-                    item.classList.remove('active')
-                })
-
-                this.classList.add('active')
-
-                if (navbar.classList.contains('navbar-mobile')) {
-                    navbar.classList.remove('navbar-mobile')
-                    let navbarToggle = select('.mobile-nav-toggle')
-                    navbarToggle.classList.toggle('bi-list')
-                    navbarToggle.classList.toggle('bi-x')
-                }
-
-                if (this.hash == '#header') {
-                    header.classList.remove('header-top')
-                    sections.forEach((item) => {
-                        item.classList.remove('section-show')
-                    })
-                    return;
-                }
-
-                if (!header.classList.contains('header-top')) {
-                    header.classList.add('header-top')
-                    setTimeout(function () {
-                        sections.forEach((item) => {
-                            item.classList.remove('section-show')
-                        })
-                        section.classList.add('section-show')
-
-                    }, 350);
-                } else {
-                    sections.forEach((item) => {
-                        item.classList.remove('section-show')
-                    })
-                    section.classList.add('section-show')
-                }
-
-                scrollto(this.hash)
+            if (this.hash == '#header') {
+                header.classList.remove('header-top');
+                sections.forEach(item => item.classList.remove('section-show'));
+                return;
             }
-        }, true)
-
-        /**
-         * Skills animation
-         */
-        let skilsContent = select('.skills-content');
-        if (skilsContent) {
-            new Waypoint({
-                element: skilsContent,
-                offset: '80%',
-                handler: function (direction) {
-                    let progress = select('.progress .progress-bar', true);
-                    progress.forEach((el) => {
-                        el.style.width = el.getAttribute('aria-valuenow') + '%'
-                    });
-                }
-            })
+            if (!header.classList.contains('header-top')) {
+                header.classList.add('header-top');
+                setTimeout(() => {
+                    sections.forEach(item => item.classList.remove('section-show'));
+                    section.classList.add('section-show');
+                }, 350);
+            } else {
+                sections.forEach(item => item.classList.remove('section-show'));
+                section.classList.add('section-show');
+            }
+            scrollto(this.hash);
         }
+    }, true);
 
-        
+    let skilsContent = select('.skills-content');
+    if (skilsContent) {
+        new Waypoint({
+            element: skilsContent,
+            offset: '80%',
+            handler: direction => {
+                let progress = select('.progress .progress-bar', true);
+                progress.forEach(el => el.style.width = el.getAttribute('aria-valuenow') + '%');
+            }
+        });
+    }
 
-    })();
-
-    /**
-     * Show video modal windows
-    */
-    var $videoSrc;
+    let $videoSrc;
     $('.video-btn').click(function () {
         $videoSrc = $(this).data("src");
     });
 
-    // when the modal is opened autoplay it
-    $('#myModal').on('shown.bs.modal', function (e) {
-        // set the video src to autoplay and not to show related video
+    $('#myModal').on('shown.bs.modal', function () {
         $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-    })
+    });
 
-    // stop playing the youtube video when I close the modal
-    $('#myModal').on('hide.bs.modal', function (e) {
-        // a poor man's stop video
+    $('#myModal').on('hide.bs.modal', function () {
         $("#video").attr('src', $videoSrc);
-    })
-
+    });
 });
